5266d08290a24bf029e7e1919530d056ce5cdb74
RavLyr <ravlybrb@gmail.com>
2025-09-14 01:06:29 +0700

    feat: implement DefaultLayout and Navbar components for improved layout structure

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-  <RouterView />
+  <DefaultLayout>
+    <RouterView />
+  </DefaultLayout>
 </template>
+
+<script setup>
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+</script>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <Navbar />
+  <main class="pt-24">
+    <slot />
+  </main>
+  <slot name="footer" />
+</template>
+
+<script setup>
+import Navbar from '@/components/ui/Navbar.vue'
+</script>
