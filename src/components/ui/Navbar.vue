179e4ac52f1dae6e6201ba1ee9f5f217df5f7028
RavLyr <ravlybrb@gmail.com>
2025-10-24 20:02:24 +0700

    fix: Update Navbar and DefaultLayout styles for improved UI consistency and readability

--- src/components/ui/Navbar.vue
+++ src/components/ui/Navbar.vue
@@ -4,33 +4,32 @@
     :class="[
       isNavbarVisible ? 'translate-x-[-50%] translate-y-0 opacity-100' : 'translate-x-[-50%] -translate-y-20 opacity-0',
       isAtTop ? 'translate-y-0 opacity-100' : '',
-      isAtTop ? 'bg-[#E4E4E4]' : 'bg-[#E4E4E4]/95 backdrop-blur-md',
+      isAtTop ? 'bg-[#C21807]' : 'bg-[#C21807]/95 backdrop-blur-md',
       isNavbarVisible ? 'scale-100' : 'scale-95'
     ]">
     <RouterLink to="/"
-      class="flex items-center gap-2 px-4 py-2 rounded-full transition font-semibold  hover:bg-[#d1f3ff] hover:text-[#26B0F0]"
-      :class="isActive('/') ? 'text-[#26B0F0] ' : ''">
-      <LaptopIcon class="w-6 h-6" />
+      class="flex items-center gap-2 px-4 py-2 rounded-full transition font-semibold     hover:bg-white/20 hover:text-[#FFE787]"
+      :class="isActive('/') ? 'text-[rgb(255,231,135)]' : ''">
       <span>Home</span>
     </RouterLink>
     <RouterLink to="/identity"
-      class="px-4 py-2 rounded-full font-medium transition  hover:bg-[#d1f3ff] hover:text-[#26B0F0]"
-      :class="isActive('/identity') ? 'text-[#26B0F0] ' : ''">
+      class="px-4 py-2 rounded-full font-medium transition     hover:bg-white/20 hover:text-[#FFE787]"
+      :class="isActive('/identity') ? 'text-[#FFE787]' : ''">
       Identity
     </RouterLink>
     <RouterLink to="/profile"
-      class="px-4 py-2 rounded-full font-medium transition  hover:bg-[#d1f3ff] hover:text-[#26B0F0]"
-      :class="isActive('/profile') ? 'text-[#26B0F0] ' : ''">
+      class="px-4 py-2 rounded-full font-medium transition     hover:bg-white/20 hover:text-[#FFE787]"
+      :class="isActive('/profile') ? 'text-[#FFE787]' : ''">
       Profile
     </RouterLink>
     <RouterLink to="/gallery"
-      class="px-4 py-2 rounded-full font-medium transition  hover:bg-[#d1f3ff] hover:text-[#26B0F0]"
-      :class="isActive('/gallery') ? 'text-[#26B0F0] ' : ''">
+      class="px-4 py-2 rounded-full font-medium transition     hover:bg-white/20 hover:text-[#FFE787]"
+      :class="isActive('/gallery') ? 'text-[#FFE787]' : ''">
       Gallery
     </RouterLink>
     <RouterLink to="/games"
-      class="px-4 py-2 rounded-full font-medium transition  hover:bg-[#d1f3ff] hover:text-[#26B0F0]"
-      :class="isActive('/games') ? 'text-[#26B0F0] ' : ''">
+      class="px-4 py-2 rounded-full font-medium transition     hover:bg-white/20 hover:text-[#FFE787]"
+      :class="isActive('/games') ? 'text-[#FFE787]' : ''">
       Fun Game
     </RouterLink>
   </nav>
